d9e79e430892c7c3abc3e44739bd3db007f9bd4f
Fly Bell <chiulynnc@gmail.com>
2020-04-30 23:20:41 +0200

    added print

--- Execute.py
+++ Execute.py
@@ -12,21 +12,25 @@ from Scrape_Web import *
 #search for COVID-19 related press releases on the Taiwan CDC website
 #since a particular date
 #with a particular set of search terms
+print("Searching for all press releases related to COVID-19")
 first_url = create_url(1, "2020.01.01", "新增 確診")
 
 #iterate through all search pages to create
 #a list of URLs of COVID-10 related press releases
 url_list = []
 next_url = first_url
+print("Downloading press releases...")
 while next_url:
     soup = make_request(next_url)
     url_list.extend(get_url(soup))
     next_url = find_next_page(soup)
 
 #create URLS.txt file (list of urls) for future reference
+print("Creating URLS.txt file")
 write_url(url_list)
 
 #create files from list of urls for future data analysis
+print("Creating individual files")
 for url in url_list:
   date, title, text = get_info(url)
   create_file(date, title, text)
