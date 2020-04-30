7887edfafce7b404592e8a0ca20309fe37a6feae
Fly Bell <chiulynnc@gmail.com>
2020-04-30 22:46:34 +0200

    rewrote main script after refractoring more methods to Scrape_Web.py

--- Execute.py
+++ Execute.py
@@ -7,29 +7,26 @@ from bs4 import BeautifulSoup #HTML parser library
 import glob #to create file name from variable
 
 #import functions
-from Parse_File import get_date, case_num
-from Scrape_Web import get_info, create_file, get_url
+from Scrape_Web import *
 
-#generate a list web pages that satisfy
-#search term "新增 確診" starting Jan 1, 2020
-page_urls = []
-part_1 = "https://www.cdc.gov.tw/Bulletin/List/MmgtpeidAR5Ooai4-fgHzQ?page="
-part_3 = "&startTime=2020.01.01&keyword=%27%E6%96%B0%E5%A2%9E%20%E7%A2%BA%E8%A8%BA%27"
-for n in range(1, 25): #known: a total of 24 pages
-    page_urls.append(part_1 + str(n) + part_3)
+#search for COVID-19 related press releases on the Taiwan CDC website
+#since a particular date
+#with a particular set of search terms
+first_url = create_url(1, "2020.01.01", "新增 確診")
 
-#generate a list of press release urls from
-#the web pages and write them into "URLS.txt"
-for page in page_urls:
-    url_list = get_url(page)
-    with open("URLS.txt", "a+") as url_file:
-        for url in url_list:
-            url_file.write(url+"\n")
+#iterate through all search pages to create
+#a list of URLs of COVID-10 related press releases
+url_list = []
+next_url = first_url
+while next_url:
+    soup = make_request(next_url)
+    url_list.extend(get_url(soup))
+    next_url = find_next_page(soup)
 
-#from the URLS.txt file, create individual text files
-#for each press release
-with open("URLS.txt", "r+") as url_file:
-    url_list = url_file.readlines()
+#create URLS.txt file (list of urls) for future reference
+write_url(url_list)
+
+#create files from list of urls for future data analysis
 for url in url_list:
-   date, title, text = get_info(url)
-   create_file(date, title, text)
+  date, title, text = get_info(url)
+  create_file(date, title, text)
