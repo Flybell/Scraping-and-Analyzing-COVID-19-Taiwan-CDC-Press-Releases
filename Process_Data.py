c2023a675c1a777a60cfe81b17d830a8f4ec10cb
Fly Bell <chiulynnc@gmail.com>
2020-05-01 02:48:14 +0200

    added matplot

--- Process_Data.py
+++ Process_Data.py
@@ -4,20 +4,36 @@ import re #regular expression
 import io #for UTF8 processing
 from bs4 import BeautifulSoup #HTML parser library
 import glob #to create file name from variable
+import matplotlib.pyplot as plt
+import numpy as np
 
 #import functions
 from Parse_File import get_date, case_num
 
-num = 0
+total_num = 0
+x_axis = []
+y_axis = []
 
 #extract data from press releases
 with open("daily_new_cases.txt", "w+") as file:
     file.write("Date" + 7*" " + "New Cases\n")
     for name in glob.glob('[0-9][0-9][0-9][0-9]-[0-9][0-9]*.txt'):
         number = case_num(name)
+        date = get_date(name)
         if number:
-            file.write(f"{get_date(name):{10}} {number}\n")
-            num = num + int(number)
-            
+            file.write(f"{date:{10}} {number}\n")
+            total_num = total_num + int(number)
+            x_axis.append(date)
+            y_axis.append(int(number))
+
 print("Created new file of daily new cases.")
-print(f"Total number of cases: {num}")
+print(f"Total number of cases: {total_num}")
+
+#plot daily new cases
+plt.plot(y_axis)
+plt.grid(axis='y', linestyle='-')
+plt.xlabel('days since first case')
+ax = plt.gca()
+ax.axes.xaxis.set_ticks([])
+#ax.axes.yaxis.set_ticks([])
+plt.show()
